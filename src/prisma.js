9082aa0f79d0b9faa7a53bc4c0e6b3d7dd03ae69
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2026-07-11 22:50:57 +0530

    feat: initialize Prisma client with MariaDB adapter instance

--- src/prisma.js
+++ src/prisma.js
@@ -2,11 +2,11 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 const adapter = new PrismaMariaDb({
-  host: '127.0.0.1',
-  port: 3306,
-  user: 'root',
-  password: '18051999@#may',
-  database: 'aws-notes',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 const prisma = new PrismaClient({ adapter });
